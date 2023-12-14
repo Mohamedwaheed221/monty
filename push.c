ceffae6aa30c8a51df33f630a31294cc74ab932b
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-12-14 06:12:45 -0800

    ./0

---
+++ push.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * push - function to push the element in the stack
+ * @stack: linked list to the stack
+ * @line_number: numbers of element enter
+ * @n: integer
+ */
+void push(stack_t **stack, unsigned int line_number, inr n)
+{
+	stack_t *node = malloc(sizeof(stack_t));
+	if (!node)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	node->n = n;
+	node->prev = NULL;
+	node->next = *stack;
+
+	if (*stack)
+	{
+		(*stack)->prev = node;
+	}
+	*stack = node;
+}
+
+/**
+ * pall - function to print the oushed item
+ * @stack: pointer to the linked list
+ * @line_number: numbers of element enter
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t temp = *stack;
+	while(temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
