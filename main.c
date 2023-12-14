b5fa64bbf907b7b7d7df329a6db9fdb4bc03de4b
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-12-14 10:44:30 -0800

    ./main

--- main.c
+++ main.c
@@ -9,12 +9,13 @@
  */
 int main(int argc, char *argv[])
 {
+	stack_t *stack = NULL;
 	if (argc != 2)
 	{
 		fprintf(stderr, "Monty");
 		exit(EXIT_FAILURE);
 	}
-	open_file(argv[1]);
+	open_file(argv[1], &stack);
 
 	return (0);
 }
